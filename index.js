34535ed42d4178299d44a887b8372710b982d9ed
Kim Foukaris <kimfoukaris@outlook.com>
2018-06-19 15:25:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function findMatching(drivers, name) {
-  let matchingDrivers = drivers.filter(function(driver) {return driver.toLowerCase() === name.toLowerCase();});
+  let matchingDrivers = drivers.filter(function(driver) {return driver.slice(0,1).toLowerCase() === name.slice(0,1).toLowerCase();});
   return matchingDrivers;
   }
+
+function fuzzyMatch() {
+  let matchingDrivers = drivers.filter(function(driver) {return driver.toLowerCase() === name.toLowerCase();});
+  return matchingDrivers;
+}
